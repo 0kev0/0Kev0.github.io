9adb3145d40c91da4db7a26938393a89a6145ba3
Lovoh17 <115592738+Lovoh17@users.noreply.github.com>
2024-04-30 21:40:20 -0600

    Actualizacion
    en about.html estan las card de presentacion + 2 footers que se agregaron

--- script.js
+++ script.js
@@ -2,3 +2,53 @@ function show(){
     document.querySelector('.hamburger').classList.toggle('open')
     document.querySelector('.navigation').classList.toggle('active')
 }
+
+function linkDescarga() {
+    window.open("https://www.mediafire.com/file/iwdyb7nw9fgsdgw/_Encuesta+CJNSP.pdf/file")
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carouselInner = document.querySelector(".inner_carousel");
+    const prevButton = document.querySelector(".prev-button");
+    const nextButton = document.querySelector(".next-button");
+    
+    let slideIndex = 0;
+    const slides = carouselInner.querySelectorAll("img");
+    const totalSlides = slides.length;
+  
+    // Función para mostrar el siguiente slide
+    function showNextSlide() {
+      slideIndex++;
+      if (slideIndex >= totalSlides) {
+        slideIndex = 0;
+      }
+      updateCarousel();
+    }
+  
+    // Función para mostrar el slide anterior
+    function showPrevSlide() {
+      slideIndex--;
+      if (slideIndex < 0) {
+        slideIndex = totalSlides - 1;
+      }
+      updateCarousel();
+    }
+  
+    // Actualizar el carrusel para mostrar el slide actual
+    function updateCarousel() {
+      carouselInner.style.transform = `translateX(-${slideIndex * 100}%)`;
+    }
+  
+    // Eventos de clic para los botones de navegación
+    prevButton.addEventListener("click", showPrevSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  
+    // Función para avanzar automáticamente
+    function autoAdvance() {
+      showNextSlide();
+    }
+  
+    // Intervalo para avanzar automáticamente cada 3 segundos
+    setInterval(autoAdvance, 3000);
+  });
+  
