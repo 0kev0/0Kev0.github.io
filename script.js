ca707dc017d5de0b9963caf1e9ae3360e377657e
0kev0 <israel_r99@outlook.com>
2024-05-03 21:24:07 -0600

    Implementacion de display web 2
    mini web 2 completa
    se agrego explicacion del glosraio de abreviaturas

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function show(){
 function linkDescarga() {
     window.open("https://www.mediafire.com/file/iwdyb7nw9fgsdgw/_Encuesta+CJNSP.pdf/file")
 }
+
 function navocult() {
   var side = document.getElementById('sidebar');
   if (side.style.display === 'none' || side.style.display === '') {
@@ -14,6 +15,31 @@ function navocult() {
       side.style.display = 'none';
   }
 }
+
+function ButtonCarrucelShow() {
+  var side = document.getElementById('carrucelBotones');
+  var txtpregunta = document.getElementById('TextPregunta');
+  var txtBtn = document.getElementById('BotonPregunta');
+  var Frase1 = document.getElementById('frase');
+  var Frase2 = document.getElementById('frase2');
+
+
+  if (side.style.display === 'none' || side.style.display === '') {
+    
+    side.style.display = 'block';
+    Frase1.style.display = 'block';
+    Frase2.style.display = 'block';
+    
+    txtpregunta.innerHTML  = '&#9733;Te tenemos cubierto...&#9733;'; // Cambia 'Nuevo texto' por el texto que desees mostrar
+    txtBtn.innerHTML  = ':)'; // Cambia 'Nuevo texto' por el texto que desees mostrar
+    
+  } else {
+    txtpregunta.innerHTML  = 'Que te parecio..???'; // Cambia 'Nuevo texto' por el texto que desees mostrar
+    txtBtn.innerHTML  = ';)'; // Cambia 'Nuevo texto' por el texto que desees mostrar
+  }
+
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -104,8 +130,151 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   // Intervalo para avanzar automáticamente cada 3 segundos
+  setInterval(autoAdvance, 6000);
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carouselInner = document.querySelector(".inner_carousel_btn");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  
+  let slideIndex = 0;
+  const slides = carouselInner.querySelectorAll("button");
+  const totalSlides = slides.length;
+
+  // Función para mostrar el siguiente slide
+  function showNextSlide() {
+    slideIndex++;
+    if (slideIndex >= totalSlides) {
+      slideIndex = 0;
+    }
+    updateCarousel();
+  }
+
+  // Función para mostrar el slide anterior
+  function showPrevSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = totalSlides - 1;
+    }
+    updateCarousel();
+  }
+
+  // Actualizar el carrusel para mostrar el slide actual
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(-${slideIndex *3}%)`;
+  }
+
+  // Eventos de clic para los botones de navegación
+  prevButton.addEventListener("click", showPrevSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+  // Función para avanzar automáticamente
+  function autoAdvance() {
+    showNextSlide();
+  }
+
+  // Intervalo para avanzar automáticamente cada 3 segundos
   setInterval(autoAdvance, 3000);
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carouselInner = document.querySelector(".inner_carousel_btn_izq");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  
+  let slideIndex = 0;
+  const slides = carouselInner.querySelectorAll("button");
+  const totalSlides = slides.length;
+
+  // Función para mostrar el siguiente slide
+  function showNextSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = totalSlides - 1;
+    }
+    updateCarousel();
+  }
+
+  // Función para mostrar el slide anterior
+  function showPrevSlide() {
+    slideIndex++;
+    if (slideIndex >= totalSlides) {
+      slideIndex = 0;
+    }
+    updateCarousel();
+  }
+
+  // Actualizar el carrusel para mostrar el slide actual
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(${slideIndex * 4}%)`;
+  }
+
+  // Eventos de clic para los botones de navegación
+  prevButton.addEventListener("click", showPrevSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+  // Función para avanzar automáticamente
+  function autoAdvance() {
+    showPrevSlide();
+  }
+
+  // Intervalo para avanzar automáticamente cada 3 segundos
+  setInterval(autoAdvance, 3000);
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carouselInner = document.querySelector(".inner_carousel_btn_drch2");
+  const prevButton = document.querySelector(".prev-button");
+  const nextButton = document.querySelector(".next-button");
+  
+  let slideIndex = 0;
+  const slides = carouselInner.querySelectorAll("button");
+  const totalSlides = slides.length;
+
+  // Función para mostrar el siguiente slide
+  function showNextSlide() {
+    slideIndex++;
+    if (slideIndex >= totalSlides) {
+      slideIndex = 0;
+    }
+    updateCarousel();
+  }
+
+  // Función para mostrar el slide anterior
+  function showPrevSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+      slideIndex = totalSlides - 1;
+    }
+    updateCarousel();
+  }
+
+  // Actualizar el carrusel para mostrar el slide actual
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(-${slideIndex *10}%)`;
+  }
+
+  // Eventos de clic para los botones de navegación
+  prevButton.addEventListener("click", showPrevSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+  // Función para avanzar automáticamente
+  function autoAdvance() {
+    showNextSlide();
+  }
+
+  // Intervalo para avanzar automáticamente cada 3 segundos
+  setInterval(autoAdvance, 3000);
+});
+
+
+
 
   function copyCodeToClipboard() {
     var codeElement = document.getElementById("code");
