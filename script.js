c354e1e54afb450272632d061bfecc3a760e3de4
0kev0 <israel_r99@outlook.com>
2024-05-05 00:20:44 -0600

    ordenado elementos, y arreglado margenesajuste a funcion copiar
    Co-Authored-By: Lovoh17 <115592738+Lovoh17@users.noreply.github.com>

--- script.js
+++ script.js
@@ -276,21 +276,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-  function copyCodeToClipboard() {
-    var codeElement = document.getElementById("code");
-    var code = codeElement.textContent;
+function copyCodeToClipboard(type, id) {
+  var codeCopied = false;
 
-    var tempInput = document.createElement("textarea");
-    tempInput.value = code;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.body.removeChild(tempInput);
+  var codeElements = document.querySelectorAll("p."+type);
+  var code = "";
+  codeElements.forEach(function(element) {
+      code += element.textContent.trim() + "\n";
+  });
 
-    var successAlert = document.getElementById("success-alert");
-    successAlert.style.display = "block";
-    setTimeout(function() {
-      successAlert.style.display = "none";
-  }, 2000); 
+  var tempInput = document.createElement("textarea");
+  tempInput.value = code;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("id");
+  document.body.removeChild(tempInput);
+  codeCopied = true
+  
+  var successAlert = document.getElementById(id);
+  if (codeCopied == true) {
+      successAlert.style.display = "block";
+      setTimeout(function() {
+          successAlert.style.display = "none";
+      }, 2000);
+  } else {
+      alert("No se encontró el elemento con ID 'success-alert'.");
+  }
+  
 }
 
 function toggleNav() {
